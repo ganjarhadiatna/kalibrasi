b01019131566b41222bc9b9db5367ef8ddf02a51
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2018-09-19 14:14:18 +0700

    changed title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Sipeja Kalibrasi</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
